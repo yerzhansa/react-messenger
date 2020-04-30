19fbf3755c600301a9f00d112e623ad0d7736bdb
Yerzhan Sagyt <erzhan.sagyt@gmail.com>
2020-04-30 14:35:42 +0600

    Initial build subtree commit

---
+++ build/precache-manifest.f655bb18c4ba3bbdb60a0bc1a47da512.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4973cd504cfbf1c64cad3dbe9d0cfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "944d3c6ce35aa78b0067",
+    "url": "/static/css/main.a3c34b65.chunk.css"
+  },
+  {
+    "revision": "54633903066c3922f7e4",
+    "url": "/static/js/2.3331d2c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3331d2c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "944d3c6ce35aa78b0067",
+    "url": "/static/js/main.19c6363f.chunk.js"
+  },
+  {
+    "revision": "3913944599b53240aa9d",
+    "url": "/static/js/runtime-main.cab33c06.js"
+  }
+]);
